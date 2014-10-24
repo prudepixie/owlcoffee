b66c5c4145fe2abfbfb5bc11eecc96fef6929bff
Matthew Carriere <matthew@dynamicowl.com>
2014-10-24 10:29:08 -0700

    added slack integration to owlcoffee!

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,8 @@
+require 'httparty'
+
 class Order < ActiveRecord::Base
+	after_create do |order|
+		payload = { text: "#{order.name} would like a #{order.order} #{order.coffee}" }.to_json
+		HTTParty.post("https://hooks.slack.com/services/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", body: payload)
+	end
 end
